2c8e2edba5c1122297c8901b870fcfa7c1c10415
DanielSolano <daniel.solano90@gmail.com>
2020-06-26 22:32:48 -0500

    Avance proyecto funcionalidad Matematicas

--- Menu/Codigo.js
+++ Menu/Codigo.js
@@ -1,7 +1,9 @@
 var b_suma = document.getElementById("botonSuma");
 var b_resta = document.getElementById("botonResta");
+var b_conjuntos = document.getElementById("botonConjuntos");
 b_suma.addEventListener("click",sumar);
 b_resta.addEventListener("click",restar);
+b_conjuntos.addEventListener("click",mostrarOcultar);
 
 function sumar(){
   var min = 1;
@@ -15,8 +17,8 @@ function sumar(){
     res_suma = resp1 + resp2;
     document.write(i+1+".-   "+resp1 +"+"+ resp2 + "=" +res_suma+"</br>");
   }
-
 }
+
 function restar(){
   var min = 1;
   var max = 20;
@@ -34,5 +36,21 @@ function restar(){
       document.write(i+1+".-   "+resp1 +"-"+ resp2 + "=" +res_resta+"</br>");
     }
   }
+}
 
+function mostrar(){
+  document.getElementById("textConjuntos").style.display ="block";
+}
+
+function ocultar(){
+  document.getElementById("textConjuntos").style.display = "none";
+}
+
+function mostrarOcultar() {
+  var escojer = document.getElementById("textConjuntos");
+  if(textConjuntos.style.display == "none"){
+    mostrar();
+  }else {
+    ocultar();
+  }
 }
